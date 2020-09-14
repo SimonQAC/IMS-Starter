dcd29e90055caea78744aa2360e4cce716f9147b
Simon Peppitt <simonworkpersonal@gmail.com>
2020-09-14 11:18:36 +0100

    added price collumn to item database and added price CRUD functionality to items

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,2 +1,2 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan2', 'harrison');
-INSERT INTO `ims`.`items` (`name`) VALUES ('book');
+INSERT INTO `ims`.`items` (`name`, `price`) VALUES ('book', 6.99);
